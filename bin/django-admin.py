9431961cfa790efa4983b29f46ea88082d218b73
viseshrp <viseshrprasad@gmail.com>
2018-02-17 16:15:46 -0500

    Add models

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/viseshprasad/Downloads/parade_interview/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- fewdly/models.py
+++ fewdly/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+import logging
 
-# Create some models here!
+logger = logging.getLogger(__name__)
+
+
+class Reviewer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(unique=True, max_length=10, default='')
+    address = models.CharField(max_length=128)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_reviewer(sender, instance, created, **kwargs):
+    if created:
+        logger.error('User created.. Now creating reviewer..')
+        Reviewer.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_reviewer(sender, instance, **kwargs):
+    logger.error('Reviewer being saved...')
+    instance.reviewer.save()
+
+
+class Restaurant(models.Model):
+    name = models.CharField(unique=True, max_length=128)
+    address = models.CharField(max_length=128)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5)
+    created_at = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Review(models.Model):
+    reviewer = models.ForeignKey(Reviewer, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    review_text = models.TextField(max_length=4000, null=True)
+
+    def __str__(self):
+        return self.rating
+
+    class Meta:
+        ordering = ['rating']
