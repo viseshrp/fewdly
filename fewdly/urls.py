c8c47470b9e41ef062903b0a3b63328250b1b697
viseshrp <viseshrprasad@gmail.com>
2018-02-18 18:14:41 -0500

    Add and refine URLs

--- fewdly/urls.py
+++ fewdly/urls.py
@@ -5,9 +5,14 @@ from . import views
 app_name = 'fewdly'
 
 urlpatterns = [
-    url(r'^reviews/(?P<user_id>[0-9]+)/$', views.Reviews.as_view(), name='review'),
-    url(r'^reviews/$', views.Reviews.as_view(), name='review_list'),
-    url(r'^restaurantlist/$', views.RestaurantList.as_view(), name='restaurant_list'),
+    url(r'^$', views.api_root),
+    url(r'^reviews/$', views.ReviewList.as_view(), name='review-list'),
+    url(r'^create-review/$', views.ReviewCreate.as_view(), name='create-review'),
+    url(r'^review/(?P<pk>[0-9]+)/$', views.ReviewDetail.as_view(), name='review'),
+    url(r'^user-reviews/(?P<user_id>[0-9]+)/$', views.get_user_review, name='user-review'),
+    url(r'^restaurants/$', views.RestaurantList.as_view(), name='restaurant-list'),
+    url(r'^create-restaurant/$', views.RestaurantCreate.as_view(), name='create-restaurant'),
     url(r'^restaurant/(?P<pk>[0-9]+)/$',
         views.RestaurantDetail.as_view(), name='restaurant'),
+    url(r'^reviewers/$', views.ReviewerList.as_view(), name='reviewer-list'),
 ]
