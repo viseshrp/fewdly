65c4f3f7d3b7756cc1c1dbbd0343421d9abeaa1e
viseshrp <viseshrprasad@gmail.com>
2018-02-18 11:22:41 -0500

    Register models with admin

--- fewdly/admin.py
+++ fewdly/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Review, Reviewer, Restaurant
 # Register your models here.
+admin.site.register(Review)
+admin.site.register(Reviewer)
+admin.site.register(Restaurant)
