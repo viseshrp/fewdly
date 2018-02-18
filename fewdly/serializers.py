f45d74df86509c1c2f020b2afe1be00e628fd6bf
viseshrp <viseshrprasad@gmail.com>
2018-02-18 11:22:29 -0500

    Add serializer

--- fewdly/serializers.py
+++ fewdly/serializers.py
@@ -1,1 +1,23 @@
 from rest_framework import serializers
+from fewdly.models import Review, Reviewer, Restaurant
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Restaurant
+        fields = '__all__'
+
+
+class ReviewerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Reviewer
+        fields = '__all__'
